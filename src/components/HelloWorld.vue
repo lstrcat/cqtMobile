f79711f4281eb93eb6698717bf6ffe9d3cdfe846
xrooo <sbr@gg.com>
2022-04-24 15:11:15 +0800

    1

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -221,7 +221,7 @@ const queryNumber = () => {
 
   </n-image>
 
-<n-carousel autoplay>
+  <n-carousel autoplay>
     <img
       class="carousel-img"
       src="/src/assets/diamond1.png"
@@ -237,42 +237,43 @@ const queryNumber = () => {
   </n-carousel>
 
 
-	<router-link style="text-decoration:none;" to='/page1'>
-    <n-card :bordered="false" content-style="padding: 0;" header-style="padding: 0;">
-    <n-layout>
-      <n-layout-header>我们的优势</n-layout-header>
-      <n-layout has-sider>
-        <n-layout-sider >
-          OUR ADVANTAGES
-        </n-layout-sider>
-        <n-layout-content >
-          <n-icon size="20" :depth="3">
-            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 512 512"><path d="M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6 0-33.9l96.4-96.4l-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136 136c9.5 9.4 9.5 24.6.1 34z" fill="currentColor"></path></svg>
-          </n-icon>   
-        </n-layout-content>
-      </n-layout>
-    </n-layout>   
-    </n-card> 
-  </router-link>
-  <n-divider />
-	<router-link style="text-decoration:none;" to='/page2'>
-    <n-card :bordered="false" content-style="padding: 0;" header-style="padding: 0;">
-    <n-layout>
-      <n-layout-header>关于我们</n-layout-header>
-      <n-layout has-sider>
-        <n-layout-sider >
-          ABOUT US
-        </n-layout-sider>
-        <n-layout-content >
-          <n-icon size="20" :depth="3">
-            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 512 512"><path d="M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6 0-33.9l96.4-96.4l-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136 136c9.5 9.4 9.5 24.6.1 34z" fill="currentColor"></path></svg>
-          </n-icon>          
-        </n-layout-content>
-      </n-layout>
-    </n-layout>  
-    </n-card>
-  </router-link>
-
+  <div style="margin-top:20px;">
+    <router-link style="text-decoration:none;" to='/page1'>
+      <n-card :bordered="false" content-style="padding: 0;" header-style="padding: 0;">
+      <n-layout>
+        <n-layout-header>我们的优势</n-layout-header>
+        <n-layout has-sider>
+          <n-layout-sider >
+            OUR ADVANTAGES
+          </n-layout-sider>
+          <n-layout-content >
+            <n-icon size="20" :depth="3">
+              <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 512 512"><path d="M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6 0-33.9l96.4-96.4l-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136 136c9.5 9.4 9.5 24.6.1 34z" fill="currentColor"></path></svg>
+            </n-icon>   
+          </n-layout-content>
+        </n-layout>
+      </n-layout>   
+      </n-card> 
+    </router-link>
+    <n-divider />
+    <router-link style="text-decoration:none;" to='/page2'>
+      <n-card :bordered="false" content-style="padding: 0;" header-style="padding: 0;">
+      <n-layout>
+        <n-layout-header>关于我们</n-layout-header>
+        <n-layout has-sider>
+          <n-layout-sider >
+            ABOUT US
+          </n-layout-sider>
+          <n-layout-content >
+            <n-icon size="20" :depth="3">
+              <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 512 512"><path d="M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6 0-33.9l96.4-96.4l-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136 136c9.5 9.4 9.5 24.6.1 34z" fill="currentColor"></path></svg>
+            </n-icon>          
+          </n-layout-content>
+        </n-layout>
+      </n-layout>  
+      </n-card>
+    </router-link>
+  </div>
 
 
   <div class="card" @click="showModalImgs = true">
