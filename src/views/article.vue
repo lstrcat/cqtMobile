c52b2ec3b1ce34123ec6ed1c1d6b4d4e4031d48d
xrooo <sbr@gg.com>
2022-04-27 09:52:41 +0800

    1

--- src/views/article.vue
+++ src/views/article.vue
@@ -18,7 +18,7 @@ const goBack = (name) => {
       <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 24 24"><path d="M16.88 2.88a1.25 1.25 0 0 0-1.77 0L6.7 11.29a.996.996 0 0 0 0 1.41l8.41 8.41c.49.49 1.28.49 1.77 0s.49-1.28 0-1.77L9.54 12l7.35-7.35c.48-.49.48-1.28-.01-1.77z" fill="currentColor"></path></svg>
     </n-icon>
   </div>
-  <p>{{route.params.title}}</p>
+  <n-h6>{{route.params.title}}</n-h6>
   <p v-html="route.params.content"></p>
   
 </template>
@@ -32,8 +32,11 @@ const goBack = (name) => {
   width: 100%;
   height: 60px;
 }
+
 div > p{
   padding-left: 20px;
   padding-right: 20px;
+  text-align: left;
+  text-indent:2em; 
 }
 </style>
